09ef88ad8a2aced8b91fce425379ca647c88f0be
Benjamin HOARAU <ben430hoarau@gmail.com>
2023-03-19 22:44:33 +0100

    not working

--- DjangoReservation/settings.py
+++ DjangoReservation/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'schools.middleware.ProcessRequestMiddleware'
 ]
 
 ROOT_URLCONF = 'DjangoReservation.urls'
