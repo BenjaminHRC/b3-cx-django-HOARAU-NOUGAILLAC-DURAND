f1fc180b5a42b7c14b7031c13029b872bfeaac6e
TNougaillac <tom.nougaillac@hotmail.fr>
2023-03-23 17:05:49 +0100

    working logout and adding it to the navbar

--- authenticate/urls.py
+++ authenticate/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.user_login, name='login'),
-    # path('login/', views.user_login, name='login'),
+    path('login/', views.user_login, name='login'),
     path('register/', views.user_register, name='register'),
     path('logout/', views.logout_view, name='logout')
 ]
